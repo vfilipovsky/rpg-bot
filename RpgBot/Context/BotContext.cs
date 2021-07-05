8b994fbb461fa55045f864a4778031e3b2396269
Vadim Filipovsky <vadim.filipovsky@gmail.com>
2021-07-05 21:03:39 +0300

    feat: added group and user entity

---
+++ RpgBot/Context/BotContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RpgBot.Entity;
+
+namespace RpgBot.Context
+{
+    public class BotContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Group> Groups { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=./Data/Database/bot.db");
+        }
+    }
+}
---
+++ RpgBot/Entity/Group.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RpgBot.Entity
+{
+    public class Group
+    {
+        public int GroupId { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
